efc1a6ceaf76ee96668ea0515f4720b0c28ac96e
p31umi <emmanuelplum3@gmail.com>
2021-04-22 16:46:25 +0100

    login page

--- src/app/inl-website/inl-website-routing.module.ts
+++ src/app/inl-website/inl-website-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { InlWebsiteComponent } from './inl-website.component';
+// import { InlWebsiteComponent } from './inl-website.component';
+import { InlLoginComponent } from './login/inl-login.component';
 
-const routes: Routes = [{ path: '', component: InlWebsiteComponent }];
+const routes: Routes = [{ path: '', component: InlLoginComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
